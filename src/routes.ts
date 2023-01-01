8ef9a578f82c1f7812577e1ddd7ff5904f68c57a
Douglas Monteiro <douglasmonteiro071@gmail.com>
2023-01-01 18:39:57 -0300

    atualizar modelos de cortes

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { CreateHaircutController } from "./controllers/haircut/CreateHaircutController";
 import { ListHaircutController } from "./controllers/haircut/ListHaircutController";
+import { UpdateHaircutController } from "./controllers/haircut/UpdateHaircutController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
@@ -18,6 +19,7 @@ router.put("/users", isAuthenticated, new UpdateUserController().handle);
 // ---ROTAS HAIRCUT---
 router.post("/haircut", isAuthenticated, new CreateHaircutController().handle);
 router.get("/haircuts", isAuthenticated, new ListHaircutController().handle);
+router.put("/haircut", isAuthenticated, new UpdateHaircutController().handle);
 
 //________________________________________________________________________________//
 export { router };
---
+++ src/services/haircut/UpdateHaircutService.ts
@@ -0,0 +1,47 @@
+import prismaClient from "../../prisma";
+
+interface HaircutRequest {
+  user_id: string;
+  haircut_id: string;
+  name: string;
+  price: number;
+  status: string | boolean;
+}
+
+class UpdateHaircutService {
+  async execute({
+    user_id,
+    haircut_id,
+    name,
+    price,
+    status = true,
+  }: HaircutRequest) {
+    const user = await prismaClient.user.findFirst({
+      where: {
+        id: user_id,
+      },
+      include: {
+        subscriptions: true,
+      },
+    });
+
+    if (user?.subscriptions?.status !== "active") {
+      throw new Error("Não autorizado");
+    }
+
+    const haircut = await prismaClient.haircut.update({
+      where: {
+        id: haircut_id,
+      },
+      data: {
+        name,
+        price,
+        status: status === true ? true : false,
+      },
+    });
+
+    return haircut;
+  }
+}
+
+export { UpdateHaircutService };
