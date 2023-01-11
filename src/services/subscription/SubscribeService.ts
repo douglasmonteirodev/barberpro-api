2862d1a9fea4b0769a6b6c0eaa4f0d7ebf9a627e
Douglas Monteiro <douglasmonteiro071@gmail.com>
2023-01-10 21:24:15 -0300

    configurando checkout

--- src/services/subscription/SubscribeService.ts
+++ src/services/subscription/SubscribeService.ts
@@ -1,11 +1,61 @@
 import prismaClient from "../../prisma";
+import Stripe from "stripe";
 
 interface SubscribeRequest {
   user_id: string;
 }
 class SubscribeService {
   async execute({ user_id }: SubscribeRequest) {
-    return user_id;
+    const stripe = new Stripe(process.env.STRIPE_API_KEY, {
+      apiVersion: "2022-11-15",
+      appInfo: {
+        name: "barberpro",
+        version: "1",
+      },
+    });
+
+    const findUser = await prismaClient.user.findFirst({
+      where: {
+        id: user_id,
+      },
+    });
+
+    let customerId = findUser.stripe_customer_id;
+
+    if (!customerId) {
+      const stripeCustomer = await stripe.customers.create({
+        email: findUser.email,
+      });
+
+      await prismaClient.user.update({
+        where: {
+          id: user_id,
+        },
+        data: { stripe_customer_id: stripeCustomer.id },
+      });
+
+      customerId = stripeCustomer.id;
+    }
+
+    const stripeCheckoutSession = await stripe.checkout.sessions.create({
+      customer: customerId,
+      payment_method_types: ["card"],
+      billing_address_collection: "required",
+      line_items: [
+        {
+          price: process.env.STRIPE_PRICE,
+          quantity: 1,
+        },
+      ],
+      mode: "subscription",
+      allow_promotion_codes: true,
+      success_url: process.env.STRIPE_SUCCESS_URL,
+      cancel_url: process.env.STRIPE_CANCEL_URL,
+    });
+
+    return {
+      sessionId: stripeCheckoutSession.id,
+    };
   }
 }
 
